c28e542e7857cc832570ab567c57623e07d666b4
annaguidagenesys <anna.guida@genesys.com>
2025-05-20 16:33:00 +0200

    Update widgets.ubs.demo.js

--- UBSPOC/widgets.ubs.demo.js
+++ UBSPOC/widgets.ubs.demo.js
@@ -21,11 +21,10 @@ function cx_getElement(id) {
     let data;
     let url = 'https://6724f040c39fedae05b35190.mockapi.io/api/AnnaG/TestUBS/?ani=' + v_customerPhone;
     const res =  fetch(url);
-    if (res.ok) {
+  //  if (res.ok) {
        data =  res.json();
       console.log("pipppo" +res);
-    }
-    console.log("pappo" + data);
+    
      //   body: JSON.stringify(data);
   
     document.getElementById("nome").innerHTML = data.name;
